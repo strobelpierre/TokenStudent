f8412750f1580a7aa35b4440a747cf8da6bd5dd5
Pierre <strobelpierre@gmail.com>
2018-04-10 16:19:08 +0200

    new/ Dashbord

---
+++ assets/js/tasks.js
@@ -0,0 +1,15 @@
+var TaskList = (function () {
+  return {
+
+    initTaskWidget: function () {
+      $('input.list-child').change(function () {
+        if ($(this).is(':checked')) {
+          $(this).parents('li').addClass('task-done')
+        } else {
+          $(this).parents('li').removeClass('task-done')
+        }
+      })
+    }
+
+  }
+}())
