d891832208c71d6b9b67300869a18ffa11d37e5d
Pierre <strobelpierre@gmail.com>
2018-04-11 11:50:14 +0200

    add/ Slack travis

--- api/controllers/PageController.js
+++ api/controllers/PageController.js
@@ -4,11 +4,22 @@
  * @description :: Server-side actions for handling incoming requests.
  * @help        :: See https://sailsjs.com/docs/concepts/actions
  */
+const sails = require('sails')
+const User = sails.models.user
 module.exports = {
   home: function (req, res) {
+    let currrentUser
     if (!req.session.user) {
       return res.redirect('/login')
     }
-    return res.view('pages/homepage')
+    User.findOne({
+      id: req.session.user.id
+    }).exec(function afterFind (err, user) {
+      if (err) {
+        sails.log.error(err)
+      }
+      currrentUser = user
+    })
+    return res.view('pages/homepage', currrentUser)
   }
 }
