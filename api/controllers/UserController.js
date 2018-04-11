32a8c6086b58ae5fdf300549ddbb2362d6e7298a
Ivachko <strobelpierre@gmail.com>
2018-04-11 10:23:00 +0200

    Feat/user (#8)
    * new/ Dashbord
    
    * new/ login
    
    * new/ login
    
    * fix/ codestyle

--- api/controllers/UserController.js
+++ api/controllers/UserController.js
@@ -4,7 +4,30 @@
  * @description :: Server-side actions for handling incoming requests.
  * @help        :: See https://sailsjs.com/docs/concepts/actions
  */
-
+const util = require('util')
+const sails = require('sails')
+const User = require('sails').HydrateModel(require('../api/models/User.js'))
 module.exports = {
-
+  login: function (req, res) {
+    sails.log.debug(util.inspect(req.body.user))
+    User.findOne({
+      email: req.body.user.email
+    }).exec(function afterFind (err, user) {
+      if (err) {
+        return res.serError(err)
+      }
+      if (!user) {
+        sails.log.error('No user find')
+        return res.notFound('user')
+      }
+      if (user.password !== req.body.user.password) {
+        return res.notFound('user')
+      }
+      /**
+       * Creation de session
+       */
+      req.session.user = user.id
+      return res.view('pages/homepage', user)
+    })
+  }
 }
