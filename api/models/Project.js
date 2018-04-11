53353b2f4cf0ce53b84ede4b6b0a10a9ca9a1fa5
Pierre <strobelpierre@gmail.com>
2018-04-11 17:08:02 +0200

    add/ Add  user

--- api/models/Project.js
+++ api/models/Project.js
@@ -11,12 +11,19 @@ module.exports = {
     title: {type: 'string', required: true},
     description: {type: 'string', required: true},
     beginDate: {type: 'string', columnType: 'datetime', required: true},
-    deadline: {type: 'string', columnType: 'datetime', required: true}
-
+    deadline: {type: 'string', columnType: 'datetime', required: true},
+    grade: {type: 'string', isIn: ['B1', 'B2', 'B3', 'I4', 'I5']},
+    teams: {
+      collection: 'Team',
+      via: 'project'
+    },
+    intervenants: {
+      collection: 'User',
+      via: 'pool'
+    }
     //  ╔═╗╦═╗╦╔╦╗╦╔╦╗╦╦  ╦╔═╗╔═╗
     //  ╠═╝╠╦╝║║║║║ ║ ║╚╗╔╝║╣ ╚═╗
     //  ╩  ╩╚═╩╩ ╩╩ ╩ ╩ ╚╝ ╚═╝╚═╝
-
     //  ╔═╗╔╦╗╔╗ ╔═╗╔╦╗╔═╗
     //  ║╣ ║║║╠╩╗║╣  ║║╚═╗
     //  ╚═╝╩ ╩╚═╝╚═╝═╩╝╚═╝
--- api/models/User.js
+++ api/models/User.js
@@ -18,6 +18,10 @@ module.exports = {
     teams: {
       collection: 'Team',
       via: 'users'
+    },
+    pool: {
+      collection: 'Project',
+      via: 'intervenants'
     }
   }
 }
