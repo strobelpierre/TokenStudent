b27b5fe32cb3e95462f2a6408a0595db34bbeed4
Pierre <strobelpierre@gmail.com>
2018-04-11 13:01:18 +0200

    fix / Pass user to homepage

--- api/controllers/PageController.js
+++ api/controllers/PageController.js
@@ -6,20 +6,27 @@
  */
 const sails = require('sails')
 const User = sails.models.user
+const util = require('util')
 module.exports = {
   home: function (req, res) {
-    let currrentUser
     if (!req.session.user) {
       return res.redirect('/login')
     }
     User.findOne({
-      id: req.session.user.id
+      id: req.session.user
     }).exec(function afterFind (err, user) {
       if (err) {
-        sails.log.error(err)
+        return res.serError(err)
       }
-      currrentUser = user
+      if (!user) {
+        sails.log.error('No user find')
+        return res.notFound('user')
+      }
+      /**
+       * Creation de session
+       */
+      sails.log.error(util.inspect(user))
+      return res.view('pages/homepage', {user})
     })
-    return res.view('pages/homepage', currrentUser)
   }
 }
