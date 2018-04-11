2a67b0f9eac61fdd7e4a2c29ba231066af9bf817
Ivachko <strobelpierre@gmail.com>
2018-04-11 16:26:27 +0200

    new/entity Team (#21)
    * new/entity Team
    
    * new/entity Team

--- api/models/User.js
+++ api/models/User.js
@@ -13,7 +13,11 @@ module.exports = {
     role: { type: 'string', required: true, isIn: ['Responsable pédagogique', 'Etudiant', 'Intervenant'] },
     email: { type: 'string', required: true },
     password: { type: 'string', required: true, encrypt: true },
-    avatar: {type: 'string', defaultsTo: 'https://www.tousvoisins.fr/img/avatar-default.png'}
-
+    avatar: {type: 'string', defaultsTo: 'https://www.tousvoisins.fr/img/avatar-default.png'},
+    grade: {type: 'string', isIn: ['B1', 'B2', 'B3', 'I4', 'I5']},
+    teams: {
+      collection: 'Team',
+      via: 'users'
+    }
   }
 }
