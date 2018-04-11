e3215605917f1a3850d60a732228e6798153df39
Pierre <strobelpierre@gmail.com>
2018-04-11 16:18:46 +0200

    new/entity Team

---
+++ api/controllers/TeamController.js
@@ -0,0 +1,10 @@
+/**
+ * TeamController
+ *
+ * @description :: Server-side actions for handling incoming requests.
+ * @help        :: See https://sailsjs.com/docs/concepts/actions
+ */
+
+module.exports = {
+
+}
---
+++ api/models/Team.js
@@ -0,0 +1,16 @@
+/**
+ * Team.js
+ *
+ * @description :: A model definition.  Represents a database table/collection/etc.
+ * @docs        :: https://sailsjs.com/docs/concepts/models-and-orm/models
+ */
+
+module.exports = {
+
+  attributes: {
+    users: {
+      collection: 'user',
+      via: 'teams'
+    }
+  }
+}
