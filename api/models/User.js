377aae718dcfc316ea09bd4fccea4a00e8a3a141
Ivachko <strobelpierre@gmail.com>
2018-04-12 15:10:57 +0200

    Feat/project new (#32)
    * fix/ProjectNew && Vue
    
    * Fix/View

--- api/models/User.js
+++ api/models/User.js
@@ -14,6 +14,7 @@ module.exports = {
     email: { type: 'string', required: true },
     password: { type: 'string', required: true, encrypt: true },
     avatar: {type: 'string', defaultsTo: 'https://www.tousvoisins.fr/img/avatar-default.png'},
+    campus: {type: 'string', isIn: ['Arras', 'Bordeaux', 'Brest', 'Grenoble', 'Lille', 'Lyon', 'Montpellier', 'Nantes', 'Paris']},
     grade: {type: 'string', isIn: ['B1', 'B2', 'B3', 'I4', 'I5']},
     rgpd: { type: 'string', defaultsTo: '0' },
     teams: {
