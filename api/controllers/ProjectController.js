414adb1ed6051cfa8d4af858c3cb55fc2c2c4092
Pierre <strobelpierre@gmail.com>
2018-04-12 03:04:16 +0200

    add/ProjectAdd

--- api/controllers/ProjectController.js
+++ api/controllers/ProjectController.js
@@ -7,6 +7,7 @@
 
 const sails = require('sails')
 const User = sails.models.user
+const Project = sails.models.project
 // const Project = sails.models.project
 const util = require('util')
 module.exports = {
@@ -51,23 +52,41 @@ module.exports = {
       if (err) {
         return res.serError(err)
       }
-
       /**
        * Vérifiction du rôle
        */
       if (user.role === 'Responsable pédagogique') {
-        return res.view('pages/newProject', {user})
+        User.find({
+          role: 'Intervenant'
+        }).decrypt().exec(function afterFind (err, Intervenants) {
+          if (err) {
+            return res.json(err)
+          }
+          return res.view('pages/newProject', {user, Intervenants})
+        })
       } else {
         return res.render('/')
       }
     })
   },
-
+  testproject: function (req, res) {
+    var project={
+      title: 't',
+      description: 'tt',
+      beginDate: '2011-04-20 10:00:00',
+      deadline: '2011-04-20 10:00:00'
+    }
+    Project.create(project).exec(function aftercreate (err) {
+      if (err) {
+        sails.log.error(err)
+      }
+      res.json(project)
+    })
+  },
   createProject: function (req, res) {
     if (!req.session.user) {
       return res.redirect('/')
     }
-    sails.log.debug(util.inspect(req.body))
     User.findOne({
       id: req.session.user
     }).decrypt().exec(function afterFind (err, user) {
@@ -86,7 +105,23 @@ module.exports = {
         var deadline = req.body.project.deadline
         var intervenants = req.body.project.intervenant
         */
-        return res.view('pages/newProject', {user})
+        sails.log.debug(util.inspect(req.body.project))
+        sails.log.debug(util.inspect(req.body.intervenant))
+        var project={
+          title: req.body.project.title,
+          description: req.body.project.description,
+          beginDate: req.body.project.beginDate,
+          deadline: req.body.project.deadline,
+          grade: req.body.project.classe,
+          intervenants: req.body.intervenant
+        }
+        Project.create(project).exec(function aftercreate (err) {
+          if (err) {
+            sails.log.error(err)
+          }
+          res.json(project)
+        })
+        // return res.view('pages/newProject', {user})
       } else {
         return res.render('/')
       }
